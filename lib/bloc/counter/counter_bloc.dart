a7cd7676ef56f0fee738c0ec532a201d35ee1355
Valentin CHEVREAU <vchevreau37@gmail.com>
2020-07-03 12:40:51 +0200

    add call of country state

--- lib/bloc/counter/counter_bloc.dart
+++ lib/bloc/counter/counter_bloc.dart
@@ -1,12 +1,23 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_demo/bloc/counter/counter_event.dart';
 import 'package:flutter_bloc_demo/bloc/counter/counter_state.dart';
+import 'package:flutter_bloc_demo/repository/counter_repository.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc(CounterState initialState) : super(initialState);
 
   @override
-  Stream<CounterState> mapEventToState(CounterEvent event) {
-    throw UnimplementedError();
+  Stream<CounterState> mapEventToState(CounterEvent event) async* {
+    CounterRepository counterRepository = CounterRepository();
+
+    if (event is IncrementCounterEvent) {
+      counterRepository.value = counterRepository.value + 1;
+      yield HasValueCounterState(counterRepository.value);
+    } else if (event is DecrementCounterEvent) {
+      counterRepository.value = counterRepository.value - 1;
+      yield HasValueCounterState(counterRepository.value);
+    } else if (event is ResetCounterEvent) {
+      yield UninitalizedCounterState();
+    }
   }
 }
