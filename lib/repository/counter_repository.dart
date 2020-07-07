ed0576f16272e3f924f46bc3b160fe877f95306e
Valentin CHEVREAU <vchevreau37@gmail.com>
2020-07-07 11:08:13 +0200

    add: singleton into counterRepository

--- lib/repository/counter_repository.dart
+++ lib/repository/counter_repository.dart
@@ -1,12 +1,23 @@
 class CounterRepository {
+  int value = 0;
 
-  int value;
-
-  static final CounterRepository _counterRepository = CounterRepository._internal();
-
+  static final CounterRepository _counterRepository =
+      CounterRepository._internal();
   factory CounterRepository() {
     return _counterRepository;
   }
 
   CounterRepository._internal();
+
+//Singleton "Java Style
+//  static CounterRepository _singleton;
+//
+//  int value;
+//
+//  CounterRepository getSingleton() {
+//    if (_singleton == null) {
+//      _singleton = CounterRepository();
+//    }
+//    return _singleton;
+//  }
 }
